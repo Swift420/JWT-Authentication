e27c60825755ad6caa91c2a38cf5009450535819
Swift420 <apollosdavid95@gmail.com>
2022-12-17 23:36:47 +0200

    Json Web Tokens

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const { check, validationResult } = require('express-validator');
 const {users} = require("../db")
 const router = require('express').Router(); 
 const bcrypt = require("bcrypt") 
-
+const JWT = require("jsonwebtoken")
 router.post("/signup", [
     check("email", "Please provvide a valid email").isEmail(),
     check("password", "Please provide a password grater than 5 characters")
@@ -39,12 +39,23 @@ router.post("/signup", [
     }
 
 
-    let hashedPassword  = await bcrypt.hash(password, 10)
+    const hashedPassword  = await bcrypt.hash(password, 10)
 
 
         users.push({
             email: email,
             password: hashedPassword
+        })
+
+
+        const token = await JWT.sign({
+            email
+        }, "secret_key_must_be_in_dotenv_file", {
+            expiresIn: 3600000
+        })
+
+        res.json({
+            token
         })
 
     res.send("signup")
