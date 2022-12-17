faf0cb3df140328a9d52431733b9f877d7eced53
Swift420 <apollosdavid95@gmail.com>
2022-12-17 21:29:01 +0200

    express validator

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,9 @@ router.post("/signup", (req,res)=> {
 
     const {password, email} = req.body 
 
-    console.log(password, email)
+    console.log(password, email);
+
+    
     res.send("signup")
 })
 
