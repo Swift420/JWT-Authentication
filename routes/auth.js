71c089ef9b40b2f40a767f4fc7e45251c316d3de
Swift420 <apollosdavid95@gmail.com>
2022-12-17 23:22:15 +0200

    Password hashing with bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,14 @@
 const { check, validationResult } = require('express-validator');
 const {users} = require("../db")
 const router = require('express').Router(); 
-
+const bcrypt = require("bcrypt") 
 
 router.post("/signup", [
     check("email", "Please provvide a valid email").isEmail(),
     check("password", "Please provide a password grater than 5 characters")
     .isLength({min: 6})
     
-],(req,res)=> {
+], async (req,res)=> {
 
     const {password, email} = req.body 
     //Validated Input
@@ -38,8 +38,23 @@ router.post("/signup", [
                  })
     }
 
+
+    let hashedPassword  = await bcrypt.hash(password, 10)
+
+    console.log(hashedPassword)
+
+        users.push({
+            email: email,
+            password: hashedPassword
+        })
+
     res.send("signup")
 })
 
 
+
+router.get("/allUsers", (req, res)=>{
+
+    res.json(users)
+})
 module.exports = router;
