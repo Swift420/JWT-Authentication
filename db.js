9b61beb2d11b705066f59a04ea96443d53f80283
Swift420 <apollosdavid95@gmail.com>
2022-12-17 22:55:04 +0200

    Mock db

---
+++ db.js
@@ -0,0 +1,11 @@
+const users = [
+    {
+        email: "apollosdavid@gmail.com",
+        password: "123456"
+    }
+]
+
+
+module.exports = {
+    users
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,25 @@
+const { check, validationResult } = require('express-validator');
+
 const router = require('express').Router(); 
 
 
-router.post("/signup", (req,res)=> {
+router.post("/signup", [
+    check("email", "Please provvide a valid email").isEmail(),
+    check("password", "Please provide a password grater than 5 characters")
+    .isLength({min: 6})
+    
+],(req,res)=> {
 
     const {password, email} = req.body 
 
-    console.log(password, email);
+    const errors = validationResult(req)
 
-    
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+
     res.send("signup")
 })
 
