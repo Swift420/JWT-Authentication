5a932e165b25699f95340c872ec1599eb6d18beb
Swift420 <apollosdavid95@gmail.com>
2022-12-17 23:02:06 +0200

    Validate if user already exists

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const { check, validationResult } = require('express-validator');
-
+const {users} = require("../db")
 const router = require('express').Router(); 
 
 
@@ -11,13 +11,31 @@ router.post("/signup", [
 ],(req,res)=> {
 
     const {password, email} = req.body 
-
+    //Validated Input
     const errors = validationResult(req)
 
     if(!errors.isEmpty()){
         return res.status(400).json({
             errors: errors.array()
         })
+    }
+
+
+    //Validate if user doesn't already exist
+
+    let user  = users.find((user) => {
+        return user.email  === email
+    });
+
+    if(user){
+        return res.status(400).json({
+            
+    "errors": [
+        {
+            "msg": "This user already exists",
+            }
+                ]
+                 })
     }
 
     res.send("signup")
